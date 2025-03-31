50be78d90ad66499654b42a1ca1f4c77fd1a469b
Scott Belcher <scottbelcher7@gmail.com>
2025-03-31 15:39:27 +0100

    gnlytest

---
+++ gnltest.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include "get_next_line.h"
+
+int main(void)
+{
+    int fd = open("test.txt", O_RDONLY);
+    if (fd < 0)
+    {
+        perror("Error opening file");
+        return (1);
+    }
+
+    char *line = get_next_line(fd);
+    if (line)
+    {
+        printf("Read line: %s", line);
+        free(line);
+    }
+    else
+        printf("get_next_line returned NULL\n");
+
+    close(fd);
+    return (0);
+}
+
